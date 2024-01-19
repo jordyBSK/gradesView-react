027e3bb43bb2beb512c776e226e8ac9f11e0059d
jordybsk <dylan.bossoku@jobtrek.ch>
2024-01-19 15:33:19 +0100

    update: grade verification

--- src/components/AverageElement.tsx
+++ src/components/AverageElement.tsx
@@ -1,17 +1,12 @@
 import GradeSvg from "./GradeSvg.tsx";
 
 export default function AverageElement({average}:{average:number}) {
-let Class = ""
 
-if (average < 1){
-    Class = "hidden"
-}else {
-    Class = "ml-1 inline-flex items-center gap-x-1.5 rounded-md px-2 py-2 text-sm font-bold text-gray-900 ring-1 ring-inset ring-gray-300"
-}
+if (average < 1)return
     return(
         <>
                 <span
-                    className={Class}
+                    className="ml-1 inline-flex items-center gap-x-1.5 rounded-md px-2 py-2 text-sm font-bold text-gray-900 ring-1 ring-inset ring-gray-300"
 
                 >
                        <GradeSvg grade={average} />
